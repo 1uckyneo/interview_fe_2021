36a3b226e9f96493be03e8454278a34225c05a7d
Neo <thrulife2gether@foxmail.com>
2021-10-06 11:42:01 +0800

    feat: getComputedStyle

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import question_100_2020 from './Question_100_2020';
+import question_25_2020 from './Question_25_2020';
 
 const main = () => {
-  question_100_2020.run();
+  // question_100_2020.run();
+  question_25_2020.run();
 };
 
 main();
