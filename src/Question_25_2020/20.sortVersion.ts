174643b78c3951e4f5c37d248849e6a7021b6b47
Neo <thrulife2gether@foxmail.com>
2021-10-07 13:32:55 +0800

    feat: sortVersion

---
+++ src/Question_25_2020/20.sortVersion.ts
@@ -0,0 +1,46 @@
+function isLeftVersionGtRight(left: string, right: string): boolean {
+  const leftNums = left.split('.').map((str) => Number.parseInt(str, 10));
+  const rightNums = right.split('.').map((str) => Number.parseInt(str, 10));
+
+  for (let i = 0; i < leftNums.length; i += 1) {
+    const currRight = rightNums[i] ?? 0;
+
+    if(leftNums[i]! === currRight) {
+      continue;
+    } else {
+      return leftNums[i]! > currRight;
+    }
+  }
+
+  return false;
+}
+
+function sortVersion(versions: string[]): string[] {
+  if(versions.length < 2) {
+    return versions;
+  }
+
+  const pivot = versions[0]!;
+  const left: string[] = [];
+  const right: string[] = [];
+
+  for(let i = 1; i < versions.length; i += 1) {
+    if(isLeftVersionGtRight(versions[i]!, pivot)) {
+      left.push(versions[i]!);
+    } else {
+      right.push(versions[i]!);
+    }
+  }
+
+  return sortVersion(left).concat([pivot]).concat(sortVersion(right));
+}
+
+export default {
+  /**
+   * 21.versions 是一个项目的版本号列表，因多人维护，不规则，动手实现一个版本号处理函数
+   */
+  run: () => {
+    const versions = ['1.45.0', '1.5', '6', '3.3.3.3.3.3.3'];
+    console.log(sortVersion(versions)); // ['1.5','1.45.0','3.3.3.3.3.3','6']
+  },
+};
--- src/Question_25_2020/index.ts
+++ src/Question_25_2020/index.ts
@@ -1,7 +1,9 @@
 import fullArrayOf2dArray from './4.fullArrayOf2dArray';
+import sortVersion from './20.sortVersion';
 
 export default {
   run: () => {
-    fullArrayOf2dArray.run(); // 4.求二维数组的全排列组合
+    // fullArrayOf2dArray.run(); // 4.求二维数组的全排列组合
+    sortVersion.run(); // 21.versions 是一个项目的版本号列表，因多人维护，不规则，动手实现一个版本号处理函数
   },
 };
