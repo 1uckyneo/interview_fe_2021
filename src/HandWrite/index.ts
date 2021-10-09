28475bbaa2e9bd0433978d366824361c52761fa4
Neo <thrulife2gether@foxmail.com>
2021-10-09 15:51:50 +0800

    feat: setTimeoutAndSetInterval

--- src/HandWrite/index.ts
+++ src/HandWrite/index.ts
@@ -4,6 +4,6 @@ import setTimeoutAndSetInterval from './setTimeoutAndSetInterval'
 export default {
   run: () => {
     // compose.run(); // 1 compose
-    setTimeoutAndSetInterval.run();
+    // setTimeoutAndSetInterval.run(); // 第二题 setTimeout 模拟实现 setInterval（setInterval 模拟实现 setTimeout）
   },
 }
--- src/HandWrite/setTimeoutAndSetInterval.ts
+++ src/HandWrite/setTimeoutAndSetInterval.ts
@@ -20,16 +20,34 @@ function mySetInterval(callback: (...arg: any) => any, ms: number = 0) {
   };
 }
 
+/* 1.setInterval 模拟实现 setTimeout
+ */
+function mySetTimeout(callback: (...arg: any) => any, ms: number = 0) {
+  const timer: ReturnType<typeof setInterval> = setInterval(() => {
+    clearInterval(timer);
+    callback();
+  }, ms);
+}
+
 export default {
+  /**
+   * 第二题 setTimeout 模拟实现 setInterval（setInterval 模拟实现 setTimeout）
+   */
   run: () => {
     /** 1.setTimeout 模拟实现 setInterval start */
-    let intervalTimeout = mySetInterval(() => {
+    const intervalTimeout = mySetInterval(() => {
       console.log('setTimeout 模拟实现 setInterval');
     }, 500);
 
     setTimeout(() => {
       intervalTimeout.cancel();
     }, 1100);
-     /** 1.setTimeout 模拟实现 setInterval end */
+    /** 1.setTimeout 模拟实现 setInterval end */
+
+    /** 2.setInterval 模拟实现 setTimeout start */
+    mySetTimeout(() => {
+      console.log('setInterval 模拟实现 setTimeout');
+    }, 1000);
+    /** 2.setInterval 模拟实现 setTimeout start */
   },
 };
